ecf9e0630e99685da237c03747dbeaca98043548
pdgarrone <pdgarrone@gmail.com>
2017-07-02 19:27:48 -0300

    Se realizaron correcciones y comentarios en la librería de buffer circular

--- libCurso/inc/ubuffer.h
+++ libCurso/inc/ubuffer.h
@@ -12,13 +12,44 @@ typedef struct Qbuffer{
 	uint8_t size;
 }Qbuffer;
 
-
+/**
+ * Función:
+ * void initCola(Qbuffer* buffer): Inicialización del buffer circular
+ *
+ * Parámetros:
+ * Qbuffer* buffer: Puntero a la estructura del buffer
+ *
+ * Devuelve:
+ * void: Nada
+ */
 void initCola(Qbuffer* buffer); //se inicializa para colocar parametros en cero
 
-
+/**
+ * Función:
+ * void producirValor(Qbuffer* buffer, uint8_t valor): Almacena un nuevo elemento en
+ * el buffer e incrementa el índice de escritura. En caso de estar completo
+ * el buffer, el nuevo elemento se almacena sobre el más antiguo.
+ *
+ * Parámetros:
+ * Qbuffer* buffer: Puntero a la estructura del buffer
+ * uint8_t valor: Valor del elemento a almacenar
+ *
+ * Devuelve:
+ * void: Nada
+ */
 void producirValor(Qbuffer* buffer, uint8_t valor); //produce el valor y lo almacena en el buffer
 
-
+/**
+ * Función:
+ * uint8_t consumirValor(Qbuffer* buffer): Se lee el valor más antiguo del buffer y
+ * se incrementa el índice de lectura.
+ *
+ * Parámetros:
+ * Qbuffer* buffer: Puntero a la estructura del buffer
+ *
+ * Devuelve:
+ * uint8_t valor: Valor recuperado del buffer. Si el buffer está vacío devuelve 0
+ */
 uint8_t consumirValor(Qbuffer* buffer); // consume valor, y reduce la cantidad de valores del buffer
 
 #endif UBUFFER_H_
--- libCurso/src/ubuffer.c
+++ libCurso/src/ubuffer.c
@@ -1,6 +1,15 @@
 #include "ubuffer.h"
 
-
+/**
+ * Función:
+ * void initCola(Qbuffer* buffer): Inicialización del buffer circular
+ *
+ * Parámetros:
+ * Qbuffer* buffer: Puntero a la estructura del buffer
+ *
+ * Devuelve:
+ * void: Nada
+ */
 void initCola(Qbuffer* buffer)
 {
 	buffer->size=0;
@@ -8,6 +17,19 @@ void initCola(Qbuffer* buffer)
 	buffer->tail=0;
 }
 
+/**
+ * Función:
+ * void producirValor(Qbuffer* buffer, uint8_t valor): Almacena un nuevo elemento en
+ * el buffer e incrementa el índice de escritura. En caso de estar completo
+ * el buffer, el nuevo elemento se almacena sobre el más antiguo.
+ *
+ * Parámetros:
+ * Qbuffer* buffer: Puntero a la estructura del buffer
+ * uint8_t valor: Valor del elemento a almacenar
+ *
+ * Devuelve:
+ * void: Nada
+ */
 void producirValor(Qbuffer* buffer, uint8_t valor)
 {
 	buffer->buffer[buffer->head]=valor;
@@ -15,9 +37,26 @@ void producirValor(Qbuffer* buffer, uint8_t valor)
 	if(buffer->size<SIZEBUFFER)
 		buffer->size++;
 	else
+	{
+		/* Si el buffer está lleno, se pierden los datos más antiguos */
 		buffer->size=SIZEBUFFER;
-}
+		buffer->tail = buffer->head; /* buffer->head ya se encuentra apuntando al elemento
+										siguiente al último valor escrito.
+										Si el buffer está lleno, ese elemento es el más antiguo */
 
+	}
+}
+/**
+ * Función:
+ * uint8_t consumirValor(Qbuffer* buffer): Se lee el valor más antiguo del buffer y
+ * se incrementa el índice de lectura.
+ *
+ * Parámetros:
+ * Qbuffer* buffer: Puntero a la estructura del buffer
+ *
+ * Devuelve:
+ * uint8_t valor: Valor recuperado del buffer. Si el buffer está vacío devuelve 0
+ */
 uint8_t consumirValor(Qbuffer* buffer)
 {
 	uint8_t valor=0;
